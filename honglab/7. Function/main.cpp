fb10b8f150c6dcf39d42d11031c4159e767c53db
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2024-01-15 12:00:33 +0900

    C++에서 함수 정의하는 방법

---
+++ honglab/7. Function/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int Add(int a, int b) {
+  return a + b;
+}
+
+void Add(int a, int b, int *c) {
+  *c = a + b;
+}
+
+int main() {
+
+  cout << Add(1, 2) << endl;
+
+  int sum;
+  Add(4, 5, &sum);
+
+  cout << sum << endl;
+
+  return 0;
+}
