ea988bcb57aec5db8f7a223f2002dd897549d2ac
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2024-01-17 21:19:15 +0900

    백준 11725번 그래프 문제

---
+++ baekjoon/graph/11725/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#define MAX 100001
+
+int arr[MAX];
+bool visited[MAX];
+vector<int> v[MAX];
+int N;
+
+void dfs(int k) {
+  visited[k] = true;
+  for(int i = 0; i < v[k].size(); i++) {
+    int next = v[k][i];
+    if(!visited[next]) {
+      arr[next] = k;
+      dfs(next);
+    }
+  }
+}
+
+int main() {
+  cin >> N;
+  for(int i = 1; i < N; i++) {
+    int x, y;
+    cin >> x >> y;
+    v[x].push_back(y);
+    v[y].push_back(x);
+  }
+
+  dfs(1);
+
+  for(int i = 2; i <= N; i++) {
+    cout << arr[i] << endl;
+  }
+
+  return 0;
+}
