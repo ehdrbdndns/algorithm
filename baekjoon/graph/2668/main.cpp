88002579f3ecd81601fb2a196221458111a7ee99
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2024-01-30 14:42:42 +0900

    백준 2668번 그래프 문제 풀이 수정

--- baekjoon/graph/2668/main.cpp
+++ baekjoon/graph/2668/main.cpp
@@ -15,31 +15,36 @@ int main() {
     list[i] = input;
   }
 
-  vector<pair<int, int>> stack; // prevParent, prevChild
-  int root;
-  int parentNode;
+  vector<int> stack; // root
   for(int i = 1; i <= N; i++) {
     vector<int> visited(N + 1, false);
-    stack.push_back({i, list[i]});
-    root = i;
-    while(stack.size()) {
-      auto prevInfo = stack.back();
-      stack.pop_back();
-      
-      if(visited[prevInfo.first] || visited[prevInfo.second]) continue;
-      visited[prevInfo.first] = true;
 
-      if(i == prevInfo.second) {
-        result.push_back(i);
-      } else {
-        stack.push_back({prevInfo.second, list[prevInfo.second]});
-      }
+    if(i == list[i]) {
+      result.push_back(i);
+      continue;
     }
+
+    stack.push_back(i);
+
+    while(stack.size()) {
+      int parent = stack.back();
+      stack.pop_back();
+
+      visited[parent] = true;
+
+      if(i == list[parent]) {
+        result.push_back(i);
+        break;
+      }
+      
+      if(visited[list[parent]]) break;
+
+      stack.push_back(list[parent]);
+    }    
   }
 
-  sort(result.begin(), result.end());
-
   cout << result.size() << '\n';
+
   for(int i = 0; i < result.size(); i++) {
     cout << result[i] << '\n';
   }
