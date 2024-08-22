ec8d1206e5042a63084290688cade380e5deb6d0
ehdrbdndns <ehdrbdndns@naver.com>
2024-08-22 17:02:40 +0900

    [leetcode] 오답 수정 (Longest Substring Without Repeating Characters)

--- leetcode/Hash Table/Longest Substring Without Repeating Characters/main.js
+++ leetcode/Hash Table/Longest Substring Without Repeating Characters/main.js
@@ -3,31 +3,31 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  let startIndex = 0;
-  let endIndex = 0;
-  let result = 1;
   const map = new Map();
+  let startIndex = 0;
+  let longestLength = 0;
 
   for (let i = 0; i < s.length; i++) {
     const c = s[i];
 
     if (map.has(c)) {
-      endIndex = map.get(c);
-      const newLength = endIndex - startIndex - 1;
-      if (result < newLength) {
-        result = newLength;
+      const length = i - startIndex;
+      longestLength = longestLength < length ? length : longestLength;
+
+      const duplicatedIndex = map.get(c);
+      // init exists map
+      for (let j = startIndex; j <= duplicatedIndex; j++) {
+        map.delete(s[j]);
       }
 
-      // map 초기화 필요
-      // endIndex 전에 있는 것들의 정보를 map에서 삭제 필요
-      for (let j = startIndex; j < endIndex; j++) {
-        map.delete(s[j])
-      }
-      startIndex = endIndex
+      startIndex = duplicatedIndex + 1;
     }
 
     map.set(c, i);
   }
 
-  return result;
-};
+  const length = s.length - startIndex;
+  longestLength = longestLength < length ? length : longestLength;
+
+  return longestLength;
+}; 
