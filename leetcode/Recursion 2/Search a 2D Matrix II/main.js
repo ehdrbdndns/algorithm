34a86ee15017259f732ed5b318cbb0e3aa08ddf6
ehdrbdndns <ehdrbdndns@naver.com>
2024-09-13 12:29:24 +0900

    [leetcode] Search a 2D Matrix 2 재시도
    Time Limit 오류남

--- leetcode/Recursion 2/Search a 2D Matrix II/main.js
+++ leetcode/Recursion 2/Search a 2D Matrix II/main.js
@@ -1,65 +1,90 @@
-let isFind = false;
-
 /**
  * @param {number[][]} matrix
  * @param {number} target
  * @return {boolean}
  */
 var searchMatrix = function (matrix, target) {
-  const n = matrix.length;
-  const m = matrix[0].length;
-  isFind = false;
-  console.log(n, m);
+  const m = matrix.length;
+  const n = matrix[0].length;
+
+  const stack = [];
 
   const visitedTable = matrix.map(row => row.map(() => false));
 
-  const startPivot = [Math.floor(m / 2), Math.floor(n / 2)];
+  const startPoint = [Math.floor(m / 2), Math.floor(n / 2)];
+  stack.push(startPoint);
 
-  const result = divide_conqure(matrix, startPivot, target, m, n, visitedTable);
+  let result = false;
+
+  while (stack.length) {
+    const [y, x] = stack.pop();
+
+    if (visitedTable[y][x]) {
+      continue;
+    }
+
+    visitedTable[y][x] = true;
+
+    let nextY = 0;
+    let nextX = 0;
+    let nextCase1Y = 0
+    let nextCase2Y = 0;
+    let nextCase1X = 0;
+    let nextCase2X = 0;
+
+    if (target === matrix[y][x]) {
+      result = true;
+      break;
+    } else if (target < matrix[y][x]) {
+      // middle
+      nextY = Math.floor(y / 2);
+      nextX = Math.floor(x / 2);
+
+      // case1 
+      nextCase1Y = y;
+      nextCase1X = x - 1;
+
+      // top
+      nextCase2Y = y - 1;
+      nextCase2X = x;
+    } else { // target > matrix[y][x]
+      // right bottom
+      nextY = Math.floor(((m - 1) - y) / 2) + y;
+      nextX = Math.floor(((n - 1) - x) / 2) + x;
+
+      // right
+      nextCase1Y = y;
+      nextCase1X = x + 1;
+
+      // bottom
+      nextCase2Y = y + 1;
+      nextCase2X = x;
+    }
+
+    if (!isOut(nextX, nextY, m, n)) {
+      stack.push([nextY, nextX]);
+    }
+
+    if (!isOut(nextCase1X, nextCase1Y, m, n)) {
+      stack.push([nextCase1Y, nextCase1X])
+    }
+
+    if (!isOut(nextCase2X, nextCase2Y, m, n)) {
+      stack.push([nextCase2Y, nextCase2X])
+    }
+  }
 
   return result;
 };
 
-const divide_conqure = (mat, pos, target, m, n, visitedTable) => {
-  const [x, y] = pos;
-
-  if (isFind) {
+const isOut = (x, y, m, n) => {
+  if (x < 0 || y < 0) {
     return true;
   }
 
-  // base case
-  if ((x >= m || y >= n) || (x < 0 || y < 0)) {
-    return false;
-  }
-
-  if (mat[y][x] === target) {
-    isFind = true;
+  if (x >= n || y >= m) {
     return true;
   }
 
-  if (visitedTable[y][x]) {
-    return false;
-  }
-
-  // visit
-  visitedTable[y][x] = true;
-
-  let sub_prob1 = false;
-  let sub_prob2 = false;
-  if (mat[y][x] < target) {
-    // right 
-    sub_prob1 = divide_conqure(mat, [x + 1, y], target, m, n, visitedTable);
-
-    // bottom
-    sub_prob2 = divide_conqure(mat, [x, y + 1], target, m, n, visitedTable);
-  } else if (mat[y][x] > target) {
-    // left
-    sub_prob1 = divide_conqure(mat, [x - 1, y], target, m, n, visitedTable);
-
-    // top
-    sub_prob2 = divide_conqure(mat, [x, y - 1], target, m, n, visitedTable);
-  }
-
-  // combine
-  return sub_prob1 || sub_prob2;
+  return false;
 }
