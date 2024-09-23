c09a140df19381b351ce23fe544d4b4240d99a40
ehdrbdndns <ehdrbdndns@naver.com>
2024-09-23 19:28:29 +0900

    [leetcode] Search a 2D Matrix 2 재풀이 (실패)

--- leetcode/Recursion 2/Search a 2D Matrix II/main.js
+++ leetcode/Recursion 2/Search a 2D Matrix II/main.js
@@ -7,84 +7,50 @@ var searchMatrix = function (matrix, target) {
   const m = matrix.length;
   const n = matrix[0].length;
 
-  const stack = [];
-
-  const visitedTable = matrix.map(row => row.map(() => false));
-
-  const startPoint = [Math.floor(m / 2), Math.floor(n / 2)];
-  stack.push(startPoint);
-
-  let result = false;
-
-  while (stack.length) {
-    const [y, x] = stack.pop();
-
-    if (visitedTable[y][x]) {
-      continue;
-    }
-
-    visitedTable[y][x] = true;
-
-    let nextY = 0;
-    let nextX = 0;
-    let nextCase1Y = 0
-    let nextCase2Y = 0;
-    let nextCase1X = 0;
-    let nextCase2X = 0;
-
-    if (target === matrix[y][x]) {
-      result = true;
-      break;
-    } else if (target < matrix[y][x]) {
-      // middle
-      nextY = Math.floor(y / 2);
-      nextX = Math.floor(x / 2);
-
-      // case1 
-      nextCase1Y = y;
-      nextCase1X = x - 1;
-
-      // top
-      nextCase2Y = y - 1;
-      nextCase2X = x;
-    } else { // target > matrix[y][x]
-      // right bottom
-      nextY = Math.floor(((m - 1) - y) / 2) + y;
-      nextX = Math.floor(((n - 1) - x) / 2) + x;
-
-      // right
-      nextCase1Y = y;
-      nextCase1X = x + 1;
-
-      // bottom
-      nextCase2Y = y + 1;
-      nextCase2X = x;
-    }
-
-    if (!isOut(nextX, nextY, m, n)) {
-      stack.push([nextY, nextX]);
-    }
-
-    if (!isOut(nextCase1X, nextCase1Y, m, n)) {
-      stack.push([nextCase1Y, nextCase1X])
-    }
-
-    if (!isOut(nextCase2X, nextCase2Y, m, n)) {
-      stack.push([nextCase2Y, nextCase2X])
-    }
-  }
+  const result = hasTarget(matrix, 0, m - 1, 0, n - 1, target);
 
   return result;
 };
 
-const isOut = (x, y, m, n) => {
-  if (x < 0 || y < 0) {
+const hasTarget = (matrix, top, bottom, left, right, target) => {
+
+  // get middle point of matrix
+  let middleX = Math.ceil((right - left) / 2) + left;
+  let middleY = Math.ceil((bottom - top) / 2) + top;
+
+  console.log(top, bottom, left, right)
+  console.log(middleY, middleX)
+  console.log("----")
+
+  // check edge
+  if (middleX < 0 || middleX >= matrix[0].length
+    || middleY < 0 || middleY >= matrix.length) {
+    return false;
+  }
+
+  // check target
+  if (matrix[middleY][middleX] === target) {
     return true;
   }
 
-  if (x >= n || y >= m) {
-    return true;
-  }
+  // divide sub problem
+  if (matrix[middleY][middleX] > target) {
+    if (middleY === bottom && middleX === right) {
+      return false;
+    }
 
-  return false;
+    // discard bottom-right matrix
+    let newBottom = middleY;
+    let newRight = middleX;
+
+    return hasTarget(matrix, top, newBottom, left, newRight, target);
+  } else { // matrix[middleY][middleX] < target
+    if (middleY === top && middleX === left) {
+      return false;
+    }
+    // discard top-left matrix
+    let newTop = middleY;
+    let newLeft = middleX;
+    return hasTarget(matrix, newTop, bottom, newLeft, right, target);
+  }
 }
