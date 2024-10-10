b851973163d047dfe5463503614e756c0e420ab9
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2024-10-10 15:52:07 +0900

    Time: 78 ms (76.81%), Space: 53.1 MB (75.8%) - LeetHub

---
+++ leetcode/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const map = new Map();
+    let startIndex = 0;
+    let longestLength = 0;
+
+    for(let i = 0; i < s.length; i++) {
+        const c = s[i];
+
+        if(map.has(c) && map.get(c) >= startIndex) {
+            const length = i - startIndex;
+            longestLength = longestLength < length ? length : longestLength;
+
+            const duplicatedIndex = map.get(c);
+
+            startIndex = duplicatedIndex + 1;
+        }
+
+        map.set(c, i);
+    }
+
+    const length = s.length - startIndex;
+    longestLength = longestLength < length ? length : longestLength;
+
+    return longestLength;
+}; 
