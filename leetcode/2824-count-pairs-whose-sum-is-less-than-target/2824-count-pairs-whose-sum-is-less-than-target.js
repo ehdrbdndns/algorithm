e2574615c22b2c2c751f158648023e0c2bda987a
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2024-11-13 23:04:19 +0900

    Time: 4 ms (8.73%), Space: 50.7 MB (91.43%) - LeetHub

--- leetcode/2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
+++ leetcode/2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -1,15 +1,27 @@
-var countPairs = function (nums, target) {
-  nums.sort((a, b) => a - b);
-  let count = 0;
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    if (nums[left] + nums[right] < target) {
-      count += right - left;
-      left++;
-    } else {
-      right--;
+var countPairs = function(nums, target) {
+    const sortedNums = nums.sort((a, b) => a - b);
+    let count = 0;
+    
+    for (let i = 0; i < sortedNums.length; i++) {
+        const elem = target - sortedNums[i];
+        
+        let low = i + 1;
+        let high = sortedNums.length;
+        
+        // Binary search to find the first element that is >= elem
+        while (low < high) {
+            const middle = low + Math.floor((high - low) / 2);
+            
+            if (sortedNums[middle] < elem) {
+                low = middle + 1;
+            } else {
+                high = middle;
+            }
+        }
+        
+        // All indices from i+1 up to `low - 1` are valid pairs
+        count += (low - i - 1);
     }
-  }
-  return count;
+
+    return count;
 };
