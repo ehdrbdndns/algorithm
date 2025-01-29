ce951def63aeffe93af2dce428c0476eb5bac0a6
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2025-01-29 22:28:32 +0900

    Time: 0 ms (100%), Space: 19.4 MB (28.95%) - LeetHub

---
+++ leetcode/0088-merge-sorted-array/0088-merge-sorted-array.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var i = 0;
+        for num in nums2 {
+            var index = i + m;
+            
+            if(index >= nums1.count) {
+                nums1.append(num)
+            } else {
+                nums1[index] = num
+            }
+
+            i += 1
+        }
+
+        nums1 = nums1.sorted { $0 < $1 }
+    }
+}
