77c901eebf52cbe405a557eabf4ef90a15802338
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2025-04-03 15:43:14 +0900

    Time: 11 ms (15.4%), Space: 17.5 MB (14.23%) - LeetHub

--- leetcode/0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ leetcode/0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        // Split the input string into words
+        istringstream iss(s);
+        vector<std::string> words;
+        string word;
+        while (iss >> word) {
+            // Reverse each word and add to the vector
+            reverse(word.begin(), word.end());
+            words.push_back(word);
+        }
+        
+        // Join the reversed words to form the final result
         string result = "";
-        vector<char> temp = {};
-        for(char c : s) {
-            if(c == ' '){
-                reverse(temp.begin(), temp.end());
-                for(char i : temp) {
-                    result += i;
-                }
-                result += " ";
-                temp = {};
-            } else {
-                temp.push_back(c);
-            }
+        for (const string& reversedWord : words) {
+            result += reversedWord + " ";
         }
-
-        reverse(temp.begin(), temp.end());
-        for(char i : temp) {
-            result += i;
-        }
-
-        return result;
+        result.pop_back();  // Remove the extra space at the end
+        
+        return result;        
     }
 };
