4f1640b7f5e768e54e1778a147e3c2e55ac235d3
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2025-05-14 12:07:46 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ leetcode/0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    let result = [];
+    
+    intervals.sort((a, b) => {
+        return a[0] - b[0]
+    })
+
+    let rear = 0;
+    let tail = 1;
+    let overlapped = intervals[rear];
+    while(tail < intervals.length) {
+        // rear의 end와 tail의 start 비교
+        let end = overlapped[1];
+        let start = intervals[tail][0];
+
+        if(end >= start) {
+            // overlapping
+            overlapped = [overlapped[0], intervals[tail][1]]
+        } else {
+            // not overlapping
+            result.push(overlapped);
+            overlapped = intervals[tail];
+        }
+
+        tail++;
+    }
+
+    result.push(overlapped);
+
+    return result;
+};
