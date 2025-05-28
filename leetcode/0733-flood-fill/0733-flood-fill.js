7f1abdb0f5dab6e72c58cbd06c78abc24fa205d2
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2025-05-28 10:12:43 +0900

    Time: 8 ms (6.84%), Space: 59.3 MB (11.37%) - LeetHub

---
+++ leetcode/0733-flood-fill/0733-flood-fill.js
@@ -0,0 +1,54 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, color) {
+    const m = image.length;
+    const n = image[0].length;
+
+    const startColor = image[sr][sc];
+    const dirList = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+    const visit = Array.from({ length: m }, () => new Array(n).fill(false))
+    visit[sr][sc] = true;
+
+    const stack = [];
+    stack.push({row: sr, col: sc});
+
+    while(stack.length) {
+        const cur = stack.pop();
+        image[cur.row][cur.col] = color;
+
+        for(let i = 0; i < dirList.length; i++) {
+            const [row, col] = dirList[i];
+
+            const nextRow = cur.row + row;
+            const nextCol = cur.col + col;
+
+            // check edge
+            if(nextRow >= m || nextRow < 0 ||
+               nextCol >= n || nextCol < 0   ) {
+                continue;
+            }
+
+            // check visit
+            if(visit[nextRow][nextCol]) {
+                continue;
+            }
+
+            // check same color
+            if(image[nextRow][nextCol] !== startColor) {
+                continue;
+            }
+
+            // visit
+            visit[nextRow][nextCol] = true;
+            stack.push({row: nextRow, col: nextCol});
+        }
+    }
+
+    return image
+};
