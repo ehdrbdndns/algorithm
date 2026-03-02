1bd70102abf3e90ec62cf53b3969c5b31545124d
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2026-03-02 16:06:12 +0900

    [level 2] Title: 타겟 넘버, Time: 42.46 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,21 +1,4 @@
 function solution(numbers, target) {
     var answer = 0;
-    
-    let stack = [] // {num, index}
-    stack.push({num: numbers[0], index: 0})
-    stack.push({num: -numbers[0], index: 0})
-    
-    while(stack.length !== 0) {
-        let {num, index} = stack.pop()
-        
-        if(index + 1 >= numbers.length) {
-            if(num === target) { answer++ }
-            continue
-        }
-        
-        stack.push({num: num + numbers[index + 1], index: index + 1})
-        stack.push({num: num - numbers[index + 1], index: index + 1})
-    }
-    
     return answer;
 }
